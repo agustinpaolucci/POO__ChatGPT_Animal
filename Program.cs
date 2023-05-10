7572d7f006bb8792a25fc0b1360a4883375699bb
agustinpaolucci <globalprint@gmail.com>
2023-05-09 21:56:38 -0300

    Actualizacion del READ.ME con consigna.

--- Program.cs
+++ Program.cs
@@ -35,12 +35,14 @@ namespace POO_ChatGPT_Animal
 
 
 /*
-POO - HERENCIA
-CONSIGNA:
-Ejercicio 1: Crear una clase "Animal" que tenga una propiedad "Nombre" y un método "HacerSonido". 
+POO - HERENCIA SOLUCIÓN A LA CONSIGNA DE CHATGPT:
 
-Crear clases "Perro" y "Gato" que hereden de "Animal" y tengan sus propios métodos "Ladrar" y "Maullar",
-respectivamente.
-
+1) Crear una clase "Animal" que tenga una propiedad "Nombre" y un método "HacerSonido".
+2) Crear clases "Perro" y "Gato" que hereden de "Animal" y tengan sus propios métodos 
+"Ladrar" y "Maullar" respectivamente.
+3) Hacer solución incluyendo: super clase, subclases o clases derivadas, atributos, métodos
+constructores CON parámetros y SIN, métodos ToString() y métodos de control. Instanciar las 
+clases que corresondan y crear los objetos. Dictar comportamientos. Crear clase abstracta. 
+Usar override cuando corresponda.
 */
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # POO_ChatGPT_Animal
+
+POO - HERENCIA SOLUCIÓN A LA CONSIGNA DE CHATGPT:
+
+1. Crear una clase "Animal" que tenga una propiedad "Nombre" y un método "HacerSonido".
+2. Crear clases "Perro" y "Gato" que hereden de "Animal" y tengan sus propios métodos "Ladrar" y "Maullar" respectivamente.
+3. Hacer solución incluyendo: super clase, subclases o clases derivadas, atributos, métodos constructores CON parámetros y SIN, métodos ToString() y métodos de control. Instanciar las clases y crear los objetos. Dictar comportamientos. Crear clase abstracta. Usar override cuando corresponda.
